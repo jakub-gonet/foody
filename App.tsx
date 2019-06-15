b1adc7ed09ad74aaf07f99e91cc87f7b3edded0e
Jakub Gonet <jakub.gonet@swmansion.com>
2019-06-15 23:37:04 +0200

    Remove failing test

--- App.tsx
+++ App.tsx
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import styles from './styles/styles'
-import RecipeInfo from './views/RecipeInfo';
+import styles from "./styles/styles";
+import RecipeInfo from "./views/RecipeInfo";
 
 const MainNavigator = createStackNavigator(
   { RecipeInfo: RecipeInfo },
-  { initialRouteName: "RecipeInfo" });
+  { initialRouteName: "RecipeInfo" }
+);
 
 const AppContainer = createAppContainer(MainNavigator);
 
 export default function App() {
-  return (
-    <AppContainer />
-  );
+  return <AppContainer />;
 }
