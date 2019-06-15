207f734e5dba371154b6fa340f57b050d7741ca7
Jakub Gonet <jakub.gonet@swmansion.com>
2019-06-15 19:03:26 +0200

    Add react-navigator and add first routes

--- App.tsx
+++ App.tsx
@@ -1,33 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import styles from './styles/styles'
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import styles from './styles/styles'
+import RecipeInfo from './views/RecipeInfo';
+
+const MainNavigator = createStackNavigator({
+  RecipeInfo: { screen: RecipeInfo },
 });
 
-interface Props { }
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const App = createAppContainer(MainNavigator);
