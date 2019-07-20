61d37737e1d948aa0b2ecf6663e1f612bfc2a7fc
Jakub Gonet <jakub.gonet@swmansion.com>
2019-07-20 23:55:23 +0200

    Add collapse button to StepsList

--- components/StepsList.tsx
+++ components/StepsList.tsx
@@ -1,8 +1,11 @@
-import React, { FunctionComponent, ReactNode } from "react";
-import { View, Text } from "react-native";
+import React, { FunctionComponent, ReactNode, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import Step from "../models/Step";
 import styles from "../styles/styles";
 import { Avatar } from "react-native-elements";
+import Collapsible from "react-native-collapsible";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 interface Props {
   steps: Step[];
@@ -10,15 +13,29 @@ interface Props {
 }
 
 const StepsList: FunctionComponent<Props> = ({ steps, topLevel }) => {
+  const [stepsCollapsed, setStepsCollapsed] = useState(false);
+  const renderCollapseButton = (shouldRender: boolean) => {
+    return (
+      shouldRender && (
+        <TouchableOpacity onPress={() => setStepsCollapsed(!stepsCollapsed)}>
+          {stepsCollapsed ? <Icon name="expand-less" style={styles.icon} /> : <Icon name="expand-more" style={styles.icon} />}
+        </TouchableOpacity>
+      )
+    );
+  };
+
   return (
     <View style={!topLevel && styles.step}>
       {steps.map((step, i) => (
         <View key={i}>
           <View style={styles.flexRow}>
             <Avatar rounded size="small" title={String(i + 1)} containerStyle={{ marginRight: 10 }} />
-            <Text>{step.title}</Text>
+            <Text style={{flexGrow: 1}}>{step.title}</Text>
+            {renderCollapseButton(step.children.length > 0)}
           </View>
-          <StepsList steps={step.children} />
+          <Collapsible collapsed={stepsCollapsed}>
+            <StepsList steps={step.children} />
+          </Collapsible>
         </View>
       ))}
     </View>
