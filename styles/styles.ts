01e4856e794086574236017237a773deba18f7d1
Jakub Gonet <jakub.gonet@swmansion.com>
2019-07-20 19:05:43 +0200

    Add new styles

--- styles/styles.ts
+++ styles/styles.ts
@@ -1,26 +1,23 @@
 import { StyleSheet } from "react-native";
 
-const PRIMARY_COLOR = "#3f7200"
+const PRIMARY_COLOR = "#3f7200";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
+    backgroundColor: "#F5FCFF"
   },
   underline: {
     borderBottomColor: PRIMARY_COLOR,
-    borderBottomWidth: 4,
+    borderBottomWidth: 4
   },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  header: {
+    fontSize: 50
+  }
 });
