e54e6cc639a2c4f042b27ebe730a5d868e3ca790
Jakub Gonet <jakub.gonet@swmansion.com>
2019-07-20 19:57:10 +0200

    Add description to Recipe

--- views/Recipe.tsx
+++ views/Recipe.tsx
@@ -1,20 +1,34 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { View, Text } from "react-native";
 import Underline from "../components/Underline";
 import StepsList from "../components/StepsList";
 import styles from "../styles/styles";
 import { Avatar } from "react-native-elements";
+import Collapsible from "react-native-collapsible";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const Recipe: FunctionComponent = () => (
-  <View>
-    <View>
-      <View style={styles.flexRow}>
-        <Text style={{ ...styles.header, flexGrow: 1 }}>Recipe title</Text>
-        <Avatar rounded size="large" title="123:45" />
+const Recipe: FunctionComponent = () => {
+  const [infoCollapsed, setInfoCollapsed] = useState(true);
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={styles.flexRow}>
+          <TouchableOpacity onPress={() => setInfoCollapsed(!infoCollapsed)}>
+            <Text style={{ ...styles.header, flexGrow: 1, marginRight: 10 }}>Recipe title</Text>
+          </TouchableOpacity>
+          <Avatar rounded size="large" title="123:45" />
+        </View>
+        <Underline />
+        <Collapsible collapsed={infoCollapsed}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venit ad extremum; Ita ne hoc quidem modo paria peccata sunt.
+            Quo studio Aristophanem putamus aetatem in litteris duxisse?
+          </Text>
+        </Collapsible>
       </View>
-      <Underline />
+      <StepsList />
     </View>
-    <StepsList />
-  </View>
-);
+  );
+};
 export default Recipe;
