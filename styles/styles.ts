a6ff82cfff2623b6c72128318bb6d2a932de0618
Jakub Gonet <jakub.gonet@swmansion.com>
2019-07-20 18:41:47 +0200

    Fix quotes in styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -1,22 +1,22 @@
 import { StyleSheet } from "react-native";
 
-const PRIMARY_COLOR = '#3f7200'
+const PRIMARY_COLOR = "#3f7200"
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
   underline: {
