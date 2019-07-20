9f783bfe566d1923e480a7e0ade4896ac3653b81
Jakub Gonet <jakub.gonet@swmansion.com>
2019-07-20 19:05:53 +0200

    Rewrite Recipe to FunctionalComponent and add timer

--- views/Recipe.tsx
+++ views/Recipe.tsx
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React, { FunctionComponent } from "react";
 import { View, Text } from "react-native";
 import Underline from "../components/Underline";
 import StepsList from "../components/StepsList";
+import styles from "../styles/styles";
+import { Avatar } from "react-native-elements";
 
-interface Props { }
-export default class Recipe extends Component<Props> {
-    render() {
-        return (
-            <View>
-                <View>
-                    <Text>Recipe title</Text>
-                    <Text>Timer</Text>
-                    <Underline />
-                </View>
-                <StepsList />
-            </View>
-        );
-    }
-}
+const Recipe: FunctionComponent = () => (
+  <View>
+    <View>
+      <View style={styles.flexRow}>
+        <Text style={{ ...styles.header, flexGrow: 1 }}>Recipe title</Text>
+        <Avatar rounded size="large" title="123:45" />
+      </View>
+      <Underline />
+    </View>
+    <StepsList />
+  </View>
+);
+export default Recipe;
